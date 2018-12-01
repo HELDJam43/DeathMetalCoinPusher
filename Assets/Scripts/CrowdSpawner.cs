fbf9c55d869face3b8b9f1f948dacaecd16157da
Michael <michael.m.goss@gmail.com>
2018-12-01 11:06:57 -0600

    Add crowd spawner

---
+++ Assets/Scripts/CrowdSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdSpawner : MonoBehaviour
+{
+    public GameObject Patron;
+    private const int crowdSize = 30;
+
+    public static List<GameObject> Patrons = new List<GameObject>();
+
+    private void Start()
+    {
+        SpawnCrowd();
+    }
+
+    private void SpawnCrowd()
+    {
+        for(int i = 0; i < crowdSize; i++)
+        {
+            Patrons.Add(RandomSpawnPatron());
+        }
+    }
+
+    private GameObject RandomSpawnPatron()
+    {
+        float x = Random.Range(0.0f, 2.0f);
+        float y = Random.Range(0.0f, 2.0f);
+
+        Vector3 position = transform.position + new Vector3(x, y, 0);
+        
+        return Instantiate(Patron, position, Quaternion.identity);
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            position.z = 0.0f;
+
+            GameObject patron = Instantiate(Patron, position, Quaternion.identity);
+            Patrons.Add(patron);
+
+            patron.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -100.0f));
+        }
+    }
+}
---
+++ Assets/Scripts/Patron.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Patron : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Rigidbody2D rigidbody2D = collision.otherCollider.gameObject.GetComponent<Rigidbody2D>();
+    }
+}
