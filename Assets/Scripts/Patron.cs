e1a883ba106cce12602d2d834c07ee863f89552f
Brent Osborne <wizardofozzy@gmail.com>
2018-12-01 14:26:37 -0600

    adding pitfall

--- Assets/Scripts/Patron.cs
+++ Assets/Scripts/Patron.cs
@@ -1,12 +1,47 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Patron : MonoBehaviour
 {
-    private Rigidbody2D rigidBody;
+    [SerializeField]
+    [Range(0.1f, 2)]
+    private float PitDeathTime;
+
+    //private float _pitDeathDeltaScale;
+    private bool _isDying = false;
+
+    private Rigidbody2D _rigidBody;
+
+    public void StartPitDeath(Vector2 dropPos)
+    {
+        if (!_isDying)
+        {
+            StartCoroutine("PitDeath", dropPos);
+        }
+    }
+
+    private IEnumerator PitDeath(Vector2 dropPos)
+    {
+        _isDying = true;
+        _rigidBody.simulated = false;
+        float _pitDeathDeltaScale = this.transform.localScale.x / PitDeathTime;
+        float distToDropPos = (new Vector3(dropPos.x, dropPos.y, 0f) - transform.position).magnitude;
+        float moveDelta = distToDropPos / PitDeathTime;
+
+        while (this.transform.localScale.x >= 0)
+        {
+            float scaleComp = transform.localScale.x - (_pitDeathDeltaScale * Time.deltaTime);
+            Vector3 newScale = new Vector3(scaleComp, scaleComp);
+            transform.localScale = newScale;
+            transform.position = Vector3.MoveTowards(transform.position, dropPos, moveDelta * Time.deltaTime);
+            yield return null;
+        }
+
+    }
 
     private void Start()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
+        _rigidBody = GetComponent<Rigidbody2D>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -16,9 +51,9 @@ public class Patron : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(rigidBody.IsSleeping())
+        if(_rigidBody.IsSleeping())
         {
-            rigidBody.mass = 0.0f;
+            _rigidBody.mass = 0.0f;
         }
     }
 }
--- Assets/Scripts/Pit.cs
+++ Assets/Scripts/Pit.cs
@@ -2,12 +2,39 @@
 
 public class Pit : MonoBehaviour
 {
+    [SerializeField]
+    private Transform BottomLeft;
+    [SerializeField]
+    private Transform BottomRight;
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
         Patron patron = collider.GetComponent<Patron>();
+
         if(patron != null)
         {
-            Destroy(patron.gameObject);
+            patron.StartPitDeath(FindPitDropPosition(patron.transform));
         }
+    }
+
+    private Vector2 FindPitDropPosition(Transform patronPos)
+    {
+        Vector2 dropPos = BottomLeft.position;
+
+        float patX = patronPos.position.x;
+
+        if (patX > dropPos.x)
+        {
+            if (patX > BottomRight.position.x)
+            {
+                dropPos.x = BottomRight.position.x;
+            }
+            else
+            {
+                dropPos.x = patX;
+            }
+        }
+
+        return dropPos;
     }
 }
