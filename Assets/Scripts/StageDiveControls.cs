7cbb0d1a3284862095e6f50161aeb412a4e38749
Brent Osborne <wizardofozzy@gmail.com>
2018-12-01 10:25:36 -0600

    Update StageDiveControls.cs

--- Assets/Scripts/StageDiveControls.cs
+++ Assets/Scripts/StageDiveControls.cs
@@ -2,15 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StageDiveControls : MonoBehaviour {
+public class StageDiveControls : MonoBehaviour 
+{
+    [SerializeField]
+    private float TimeBetweenStageDivers;
+
+    // the amount of time before a new stage diver is spaw
+    private float diveTimer;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+    {
+        ResetDiveTimer();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update () 
+    {
+        UpdateDiveTimer();
+
+        if (diveTimer < 0f)
+        {
+            if (WasDiveButtonPressed())
+            {
+                StartStageDive();
+                ResetDiveTimer();
+            }
+        }
+    }
+
+    private bool WasDiveButtonPressed()
+    {
+        // TODO - if we want to add additional control schemes, do it here.
+        bool wasPressed = false;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            wasPressed = true;
+        }
+        return wasPressed;
+    }
+
+    private void ResetDiveTimer()
+    {
+        diveTimer = TimeBetweenStageDivers;
+    }
+
+    private void UpdateDiveTimer()
+    {
+        diveTimer -= Time.deltaTime;
+    }
+
+    private void StartStageDive()
+    {
+        // TODO - do things
+    }
 }
