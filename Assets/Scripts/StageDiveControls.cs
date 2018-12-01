39ef706d84001a04e9cbc041e6b856bcbe7874f0
Brent Osborne <wizardofozzy@gmail.com>
2018-12-01 11:48:51 -0600

    adding stage diving

--- Assets/Scripts/StageDiveControls.cs
+++ Assets/Scripts/StageDiveControls.cs
@@ -7,26 +7,57 @@ public class StageDiveControls : MonoBehaviour
     [SerializeField]
     private float TimeBetweenStageDivers;
 
-    // the amount of time before a new stage diver is spaw
-    private float diveTimer;
+    [SerializeField]
+    private float StageMovementSpeed;
 
-	// Use this for initialization
-	void Start () 
+    [SerializeField]
+    private float StageDiveForce;
+
+    [SerializeField]
+    private GameObject StageDiverPrefab;
+
+    [SerializeField]
+    private Transform LeftStageBoundary;
+
+    [SerializeField]
+    private Transform RightStageBoundary;
+
+    private float _diveTimer;
+    private Rigidbody2D _activeStageDiver;
+    private float _stageDiverMovementDirection = -1;
+
+    private bool _isActiveStageDiverAvailable
+    {
+        get
+        {
+            return _activeStageDiver != null;
+        }
+    }
+
+
+    private void Start () 
     {
         ResetDiveTimer();
 	}
 	
-	// Update is called once per frame
-	void Update () 
+	private void Update () 
     {
         UpdateDiveTimer();
 
-        if (diveTimer < 0f)
+        if (_isActiveStageDiverAvailable)
         {
+            MoveStageDiver();
             if (WasDiveButtonPressed())
             {
                 StartStageDive();
                 ResetDiveTimer();
+            }
+        }
+        else
+        {
+            if (_diveTimer <= 0f)
+            {
+                SpawnStageDiver();
             }
         }
     }
@@ -39,21 +70,67 @@ public class StageDiveControls : MonoBehaviour
         {
             wasPressed = true;
         }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            wasPressed = true;
+        }
         return wasPressed;
     }
 
     private void ResetDiveTimer()
     {
-        diveTimer = TimeBetweenStageDivers;
+        _diveTimer = TimeBetweenStageDivers;
     }
 
     private void UpdateDiveTimer()
     {
-        diveTimer -= Time.deltaTime;
+        if (_diveTimer > 0)
+        {
+            _diveTimer -= Time.deltaTime;
+        }
+    }
+
+    private void MoveStageDiver()
+    {
+        Vector3 newPos = _activeStageDiver.transform.position;
+        newPos.x = newPos.x + ((StageMovementSpeed * Time.deltaTime) * _stageDiverMovementDirection);
+
+        if (_stageDiverMovementDirection < 0 && newPos.x <= LeftStageBoundary.position.x)
+        {
+            newPos.x = LeftStageBoundary.position.x;
+            _stageDiverMovementDirection *= -1f;
+        }
+        else if (_stageDiverMovementDirection > 0 && newPos.x >= RightStageBoundary.position.x)
+        {
+            newPos.x = RightStageBoundary.position.x;
+            _stageDiverMovementDirection *= -1f;
+        }
+
+        _activeStageDiver.transform.position = newPos;
+    }
+
+    private void SpawnStageDiver()
+    {
+        // Find a random spot in between the two boundaries
+        float randX = Random.Range(LeftStageBoundary.position.x + 1f, RightStageBoundary.position.x - 1f);
+        Vector3 stageDiverPos = new Vector3(randX, LeftStageBoundary.position.y);
+        GameObject temp = Instantiate(StageDiverPrefab);
+        _activeStageDiver = temp.GetComponent<Rigidbody2D>();
+        _activeStageDiver.simulated = false;
+        _activeStageDiver.transform.position = stageDiverPos;
+
+        // TODO - Enable the Arrow image
     }
 
     private void StartStageDive()
     {
-        // TODO - do things
+        // TODO - Disable the Arrow image
+
+        // Send the stage dive
+        _activeStageDiver.simulated = true;
+        _activeStageDiver.AddForce(new Vector2(0f, -StageDiveForce));
+
+        _activeStageDiver = null;
+        ResetDiveTimer();
     }
 }
