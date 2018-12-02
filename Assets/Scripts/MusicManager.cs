21660cbba8d54a3e7b4a9527b20f14b40f00fcd7
Brent Osborne <wizardofozzy@gmail.com>
2018-12-01 21:51:34 -0600

    lights on tempo

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -4,28 +4,60 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour 
 {
+    public static float BeatsPerSecond;
+
     public AudioSource Source;
+
     public AudioClip IntroLoop;
+    private float IntroBPS = 8f / 12.8f;
+
     public AudioClip MainLoop;
+    private float MainBPS = 8f / 5.3f;
+
+    public AudioClip FastLoop;
+    private float FastBPS = 32f / 9.14f;
 
 	// Use this for initialization
 	void Start () 
     {
-        Source.clip = IntroLoop;
-        Source.loop = false;
-        Source.time = 6f;
-        Source.Play();
-	}
+        PlayIntro();
+    }
 	
 	// Update is called once per frame
 	void Update () 
     {
 		if (Source.clip == IntroLoop && Source.time >= Source.clip.length)
         {
-            Source.clip = MainLoop;
-            Source.time = 0f;
-            Source.loop = true;
-            Source.Play();
+            PlayMain();
         }
     }
+
+    private void PlayIntro()
+    {
+        Source.clip = IntroLoop;
+        Source.loop = false;
+        Source.time = 6f;
+        Source.Play();
+
+        BeatsPerSecond = IntroBPS;
+    }
+    private void PlayMain()
+    {
+        Source.clip = MainLoop;
+        Source.time = 0f;
+        Source.loop = true;
+        Source.Play();
+
+        BeatsPerSecond = MainBPS;
+    }
+    private void PlayFast()
+    {
+        Source.clip = FastLoop;
+        Source.time = 0f;
+        Source.loop = true;
+        Source.Play();
+
+        BeatsPerSecond = FastBPS;
+    }
+
 }
