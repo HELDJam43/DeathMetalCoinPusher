791d0dcd864d2b6dce995f44ab72bfbdfef74d1b
Brent Osborne <wizardofozzy@gmail.com>
2018-12-02 15:33:38 -0600

    singer demon switch

---
+++ Assets/Scripts/RitualController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RitualController : MonoBehaviour 
+{
+    public GameObject SingerObject;
+    public GameObject DemonObject;
+
+    private bool _isSingerActive;
+
+	// Use this for initialization
+	void Start () 
+    {
+        _isSingerActive = true;
+        SetSingerActive();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (_isSingerActive != !GameManager.Instance.IsBonusMode)
+        {
+            _isSingerActive = !GameManager.Instance.IsBonusMode;
+            if (_isSingerActive)
+            {
+                SetSingerActive();
+            }
+            else
+            {
+                SetDemonActive();
+            }
+        }
+    }
+
+    private void SetSingerActive()
+    {
+        SingerObject.SetActive(true);
+        DemonObject.SetActive(false);
+    }
+
+    private void SetDemonActive()
+    {
+        SingerObject.SetActive(false);
+        DemonObject.SetActive(true);
+    }
+}
