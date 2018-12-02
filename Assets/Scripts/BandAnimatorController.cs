4b09755a98b721d75c6015d2eb3db4836ec27e3d
Brent Osborne <wizardofozzy@gmail.com>
2018-12-02 14:20:59 -0600

    added singer

---
+++ Assets/Scripts/BandAnimatorController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BandAnimatorController : MonoBehaviour 
+{
+    [SerializeField]
+    private Animation Animator;
+
+    private float _currentBPS = 1;
+
+    // Use this for initialization
+    private void Start()
+    {
+        UpdateBPS();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (MusicManager.BeatsPerSecond != _currentBPS)
+        {
+            UpdateBPS();
+        }
+    }
+
+    private void UpdateBPS()
+    {
+        _currentBPS = MusicManager.BeatsPerSecond;
+
+        foreach (AnimationState state in Animator)
+        {
+            state.speed = _currentBPS;
+        }
+    }
+}
