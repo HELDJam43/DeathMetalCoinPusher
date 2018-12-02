980724543af566409460cfda394136a97a432f33
Brent Osborne <wizardofozzy@gmail.com>
2018-12-01 18:12:17 -0600

    adding textmeshpro

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PointsAddedEvent : UnityEvent<int>
+{
+
+}
+
+public class GameManager : MonoBehaviour 
+{
+    public static GameManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    public PointsAddedEvent OnPointsAdded;
+
+    public void AddPoints(int points)
+    {
+        _currentPoints += points;
+        OnPointsAdded.Invoke(points);
+    }
+
+    private static GameManager _instance;
+    private int _currentPoints;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Debug.LogWarning("Two GameManagers in the scene!");
+        }
+
+        if (OnPointsAdded == null)
+        {
+            OnPointsAdded = new PointsAddedEvent();
+        }
+
+        _currentPoints = 0;
+    }
+
+    // Use this for initialization
+    void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UI/UIScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIScore : MonoBehaviour 
+{
+    private int _currentScore;
+
+    private TextMeshProUGUI _text;
+
+    public void OnScoreUpdated(int currentScore)
+    {
+        _currentScore = currentScore;
+        _text.text = ("SCORE: " + _currentScore);
+    }
+
+    // Use this for initialization
+    void Start () 
+    {
+        OnScoreUpdated(0);
+	}
+}
